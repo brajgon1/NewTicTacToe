f178aa980feb037f27922525ea60a1f53113f0f2
Brandon Gonzalez <brajgon@gmail.com>
2024-05-02 21:22:37 -0600

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Square from "./Square";
+
+
+function App() {
+  const propVariable = "This is a Prop";
+  return (
+    <div className="App">
+      <Square propVar={propVariable} />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Square(props) {
+    return (
+        <div className="square">
+            {props.propsVar}
+            <p>Hello</p>
+        </div>
+    );
+};
+
+export default Square;
